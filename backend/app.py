83c56911f9828a5aeb51d11a1f4398f08e264af5
Christopher <christopher.buss+github@protonmail.com>
2021-02-06 13:01:37 +0000

    added text detection

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import boto3
 
 app = Flask(__name__)
 
@@ -7,8 +8,29 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-def save_receipt():
-    
+
+def save_receipt(photo):
+    bucket = 'bucket'
+    detect_text(bucket, photo)
+    pass
+
+
+def detect_text(bucket, photo) -> int:
+    client = boto3.client('rekognition')
+    response = client.detect_text(Image={'S3Object': {'Bucket': bucket, 'Name': photo}})
+
+    text_detections = response['TextDetections']
+    print('Detected text\n----------')
+    for text in text_detections:
+        print('Detected text:' + text['DetectedText'])
+        print('Confidence: ' + "{:.2f}".format(text['Confidence']) + "%")
+        print('Id: {}'.format(text['Id']))
+        if 'ParentId' in text:
+            print('Parent Id: {}'.format(text['ParentId']))
+        print('Type:' + text['Type'])
+        print()
+    return len(text_detections)
+
 
 if __name__ == '__main__':
     app.run()
